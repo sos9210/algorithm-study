ca9556a03b2a6e19ad111c2b2517d3b878f299bf
sungsanghyun <sos9210@naver.com>
2022-02-07 00:06:14 +0900

    조합의 경우수 (sec08_06) 설명주석 추가

--- src/sec08/Sec08_06.java
+++ src/sec08/Sec08_06.java
@@ -2,16 +2,17 @@ package sec08;
 
 import java.util.Scanner;
 
+//조합의 경우수(메모이제이션)
 public class Sec08_06 {
     static int[][] dy;
     public int DFS(int n, int r){
-        if(dy[n][r] > 0){
+        if(dy[n][r] > 0){       //이미 탐색한경우 배열에서 찾아와서 바로 반환한다.
             return dy[n][r];
         }
-        if(n == r || r == 0){
+        if(n == r || r == 0){       // n과r이 같거나 r이0 이면 1을 반환한다.(트리 종착점)
             return 1;
-        }else{
-            return dy[n][r] = DFS(n-1, r) + DFS(n-1,r-1);
+        }else{//    nCr ... n=5, r=3일때 트리그래프로 뻗어나가며  5C3 -> ((4C2 -> 3C2, 3C1), (4C3 -> 3C3, 3C2)) ....생략
+            return dy[n][r] = DFS(n-1, r) + DFS(n-1,r-1);   //탐색한부분을 배열에 딤고 반환한다
         }
     }
     public static void main(String[] args) {
