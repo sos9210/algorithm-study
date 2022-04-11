a9f5b9b36fe54cc2d025016a25a45c26d6a647e2
sungsanghyun <sos9215@gmail.com>
2022-04-11 21:41:54 +0900

    4/10

---
+++ src/study/B2580_bew.java
@@ -0,0 +1,72 @@
+package study;
+
+import java.util.Scanner;
+
+public class B2580_bew {
+    static int[][] sudoku = new int[9][9];
+
+    public static void DFS(int row,int col){
+        if(col == 9){
+            DFS(row+1,0);
+            return;
+        }
+        if(row == 9){
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(sudoku[i][j] + " ");
+                }
+                System.out.println();
+            }
+            System.exit(0);
+        }
+        if(sudoku[row][col] == 0){
+            for (int i = 1; i <= 9; i++) {
+                if (isCheck(row, col,i)) {
+                    sudoku[row][col] = i;
+                    DFS(row,col+1);
+                }
+            }
+            sudoku[row][col] = 0;
+            return;
+        }
+        DFS(row,col+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sudoku[i][j] = sc.nextInt();
+            }
+        }
+
+       DFS(0,0);
+    }
+
+    private static boolean isCheck(int row,int col,int chkNum){
+
+        for (int i = 0; i < 9; i++) {
+            if(sudoku[row][i] == chkNum){
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if(sudoku[i][col] == chkNum){
+                return false;
+            }
+        }
+
+        int chkX = (row/3)*3;
+        int chkY = (col/3)*3;
+        for (int i = chkX; i < chkX+3; i++) {
+            for (int j = chkY; j < chkY+3; j++) {
+                if(sudoku[i][j] == chkNum){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
