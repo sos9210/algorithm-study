263c52ffb47a797853c4285a336e70b51931c13e
sungsanghyun <sos9210@naver.com>
2022-04-30 01:02:58 +0900

    04/29

--- src/study/B14923.java
+++ src/study/B14923.java
@@ -5,8 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 
 class Hongik {
-    int x,y,time;
-    int magic = 1;
+    int x,y,time,magic;
     public Hongik(int x, int y,int time, int magic){
         this.x = x;
         this.y = y;
@@ -15,33 +14,35 @@ class Hongik {
     }
 }
 public class B14923 {
-    static int N,M;
+    static int N,M,endN,endM;
     static int[][] map;
     static int[] nx = {0,0,1,-1};
     static int[] ny = {1,-1,0,0};
-    static boolean[][] visit;
-    static int min = Integer.MAX_VALUE;
+    static boolean[][][] visit;
     private static void BFS(Hongik hhhh){
         Queue<Hongik> h = new LinkedList<>();
         h.offer(hhhh);
-        visit[hhhh.x][hhhh.y] = true;
+        visit[1][hhhh.x][hhhh.y] = true;
+        visit[2][hhhh.x][hhhh.y] = true;
 
         while (!h.isEmpty()){
             Hongik hongik = h.poll();
-            if(map[hongik.x][hongik.y] == 2){
-                min = Math.min(min, hongik.time);
+            if(hongik.x == endN && hongik.y == endM){
+                System.out.println(hongik.time);
+                System.exit(0);
             }
             for (int i = 0; i < 4; i++) {
                 int dx = hongik.x + nx[i];
                 int dy = hongik.y + ny[i];
-                if(dx >= 0 && dy >= 0 && dx < N && dy < M && !visit[dx][dy]){
-                    if(map[dx][dy] == 1 && hongik.magic == 1) {                 //벽인경우 마법사용하여 이동
-                        h.offer(new Hongik(dx,dy,hongik.time+1,hongik.magic-1));
+                if(dx >= 0 && dy >= 0 && dx < N && dy < M && !visit[hongik.magic][dx][dy]){
+                    if(map[dx][dy] == 1 && hongik.magic == 0) {                 //벽인경우 마법사용하여 이동
+                        h.offer(new Hongik(dx,dy,hongik.time+1,hongik.magic+1));
+                        visit[hongik.magic+1][dx][dy] = true;
                     }
-                    if(map[dx][dy] == 0 || map[dx][dy] == 2 && map[dx][dy] != 1) {//벽이 아닌경우이고 출구거나 빈공간일 경우
+                    if(map[dx][dy] == 0 && map[dx][dy] != 1) {//벽이 아닌경우이고 빈공간일 경우
                         h.offer(new Hongik(dx,dy, hongik.time+1, hongik.magic));
+                        visit[hongik.magic][dx][dy] = true;
                     }
-                    visit[dx][dy] = true;
                 }
             }
         }
@@ -52,18 +53,18 @@ public class B14923 {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        Hongik hongik = new Hongik(sc.nextInt()-1,sc.nextInt()-1,0,1);
+        Hongik hongik = new Hongik(sc.nextInt()-1,sc.nextInt()-1,0,0);
         map = new int[N][M];
-        visit = new boolean[N][M];
-        map[sc.nextInt()-1][sc.nextInt()-1] = 2;
+        visit = new boolean[3][N][M];
+        endN = sc.nextInt()-1;
+        endM = sc.nextInt()-1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(map[i][j] != 2) map[i][j] = sc.nextInt();
+                map[i][j] = sc.nextInt();
             }
         }
 
         BFS(hongik);
-        if(min == Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(min);
+        System.out.println(-1);
     }
 }
