0c6e5c4bcef37e0d0022afc25fdc36a7e0336792
sungsanghyun <sos9215@gmail.com>
2022-05-17 00:07:19 +0900

    05/16

---
+++ src/programmers/P12916.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class P12916 {
+    static boolean solution(String s) {
+        s = s.toLowerCase();
+        char[] c = s.toCharArray();
+        int p = 0;
+        int y = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(c[i] == 'p') ++p;
+            if(c[i] == 'y') ++y;
+        }
+        if(p == y) return true;
+        else return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("pPoooyY"));
+    }
+}
