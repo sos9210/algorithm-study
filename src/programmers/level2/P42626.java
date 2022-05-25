ecd47b7ff2a8e8d67699dc345b0ceba666db8e1f
sungsanghyun <sos9215@gmail.com>
2022-05-26 00:42:38 +0900

    05/25 코드수정

--- src/programmers/level2/P42626.java
+++ src/programmers/level2/P42626.java
@@ -16,12 +16,13 @@ public class P42626 {
             int min1 = q.poll();
             int min2 = q.poll();
             int scov = min1 + (min2 * 2);
-            q.offer(scov);
             answer++;   //횟수증가
+            q.offer(scov);
+            //!모든음식!이 K이상이면 return
+            if(q.peek() >= K) return answer;
         }
         //최종스코빌지수가 K보다 작으면 return -1
-        if(q.peek() < K) return -1;
-        return answer;
+        return -1;
     }
     public static void main(String[] args) {
         System.out.println(solution(new int[]{1,2},5));
